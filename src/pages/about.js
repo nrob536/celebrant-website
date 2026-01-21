b4a426ac401e29ee67a01f6f972a72fe35d24fad
Nestor M. Robinson <nrob536@aucklanduni.ac.nz>
2026-01-21 21:09:05 +1300

    Small change to About section

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,7 @@ export default function About() {
     <Layout title="About Me">
       <main className="container margin-top--lg">
         <h1>About Me</h1>
-        <p>Love is the thread that binds us and I’m endlessly inspired by its power to bring people together in the most beautiful way. I believe a ceremony is more than a moment; it’s a sacred waving of two lives, surrounded by whānau, laughter, and the whispers of legacy. Every love story I meet is as unique and breathtaking as a snowflake- delicate, powerful and beautifully one of a kind. It’s my joy and honour to help bring those stories to life, crafting ceremonies that reflect the south of your journey and the magic of your connection. If you are looking for a celebrant who brings heart, soul and a little sparkle, I’d be honoured to walk beside you and help make your day truly unforgettable.</p>
+        <p>Love is the thread that binds us and I’m endlessly inspired by its power to bring people together in the most beautiful way. I believe a ceremony is more than a moment; it’s a sacred waving of two lives, surrounded by whānau, laughter, and the whispers of legacy. Every love story I meet is as unique and breathtaking as a snowflake- delicate, powerful and beautifully one of a kind. It’s my joy and honour to help bring those stories to life, crafting ceremonies that reflect your journey and the magic of your connection. If you are looking for a celebrant who brings heart, soul and a little sparkle, I’d be honoured to walk beside you and help make your day truly unforgettable.</p>
         
       </main>
     </Layout>
